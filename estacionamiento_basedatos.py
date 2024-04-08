62b89aec953603052769ee861b637a3eb05bd7b2
Matías Saavedra Martínez <mathiu3354@gmail.com>
2024-04-08 05:24:31 -0400

    Primer comit

---
+++ estacionamiento_basedatos.py
@@ -0,0 +1,318 @@
+import sqlite3
+import hashlib
+
+
+
+
+
+# Clase para acceder a metodos en la Base de datos
+class AccesoBaseDatos:
+    def __init__(self):
+        # Conexión a la base de datos
+        self.conn = sqlite3.connect('estacionamientoapp.db')
+        self.cursor = self.conn.cursor()
+        self.crear_tablas()
+    def crear_tablas(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id INTEGER PRIMARY KEY,
+                nombre TEXT,
+                apellido TEXT,
+                fecha_nacimiento DATE,
+                email TEXT UNIQUE,
+                contraseña TEXT,
+                estado_bloqueo BOOLEAN DEFAULT 0
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS vehiculos (
+                id INTEGER PRIMARY KEY,
+                usuario_id INTEGER,
+                marca TEXT,
+                modelo TEXT,
+                patente TEXT(6) UNIQUE,
+                FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Multas (
+                id INTEGER PRIMARY KEY,
+                usuario_id INTEGER,
+                admin_id INTEGER,
+                accion TEXT,
+                fecha_accion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+                FOREIGN KEY(admin_id) REFERENCES administradores(id)
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS estacionamientos (
+                id INTEGER PRIMARY KEY,
+                direccion TEXT UNIQUE,
+                Discapacitados BOOLEAN DEFAULT 0,
+                Disponible BOOLEAN DEFAULT 0,
+                usuario_id INTEGER,
+                vehiculo_id INTEGER,
+                solicitudes INTEGER DEFAULT 0,
+                FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+                FOREIGN KEY(vehiculo_id) REFERENCES vehiculos(id)
+            )
+        ''')
+        self.conn.commit()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS administradores (
+                id_credenciales INTEGER PRIMARY KEY,
+                nombre_completo TEXT,
+                email TEXT UNIQUE,
+                fecha_nacimiento DATE,
+                usuario_id INTEGER,
+                FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+            )
+        ''')
+        try:
+            self.cursor.execute('''
+                    ALTER TABLE administradores ADD COLUMN contraseña TEXT 
+                ''')
+            self.conn.commit()
+        except sqlite3.OperationalError:
+
+            pass
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tarifas (
+            id INTEGER PRIMARY KEY,
+            usuario_id INTEGER,
+            estacionamiento_id INTEGER,
+            tarifa NUMERIC,
+            tiempo_inicio TIMESTAMP,
+            tiempo_fin TIMESTAMP,
+            total_pagar NUMERIC,
+            FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+            FOREIGN KEY(estacionamiento_id) REFERENCES estacionamientos(id)
+            );''')
+        self.conn.commit()
+
+    #Función para el inicio de sesión
+
+    def iniciar_sesion(self,email, contraseña):
+        # Hashear la contraseña para compararla con la almacenada en la base de datos
+        contraseña_hasheada = hashlib.sha256(contraseña.encode()).hexdigest()
+
+        # Buscar el usuario en la base de datos
+        self.cursor.execute('''
+            SELECT id, nombre FROM usuarios WHERE email = ? AND contraseña = ?
+        ''', (email, contraseña_hasheada))
+        usuario = self.cursor.fetchone()
+
+        if usuario:
+            print(f"Bienvenido, {usuario[1]}")
+            return usuario[0]  # Devolver el ID del usuario
+        else:
+            print("Credenciales inválidas")
+            return None
+
+    # Función para crear un nuevo usuario con su vehículo
+    def crear_usuario(self,nombre, apellido, fecha_nacimiento, email, contraseña, patente, marca, modelo):
+        # Hashear la contraseña antes de almacenarla en la base de datos
+        contraseña_hasheada = hashlib.sha256(contraseña.encode()).hexdigest()
+
+        # Insertar el nuevo usuario en la tabla de usuarios
+        self.cursor.execute('''
+            INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, email, contraseña)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (nombre, apellido, fecha_nacimiento, email, contraseña_hasheada))
+
+        # Obtener el ID del usuario recién creado
+        usuario_id = self.cursor.lastrowid
+
+        # Insertar el vehículo asociado al nuevo usuario en la tabla de vehículos
+        self.cursor.execute('''
+            INSERT INTO vehiculos (usuario_id, marca, modelo, patente)
+            VALUES (?, ?, ?, ?)
+        ''', (usuario_id, marca, modelo, patente))
+
+        self.conn.commit()
+        print("Usuario y vehículo creados exitosamente")
+
+    # Función para solicitar un estacionamiento
+    def solicitar_estacionamiento(self,usuario_id):
+        # Solicitar la patente del vehículo
+        patente = input("Por favor, ingresa la patente de tu vehículo (6 caracteres): ").strip().upper()
+
+        # Verificar si el vehículo existe en la base de datos
+        self.cursor.execute('''
+            SELECT id FROM vehiculos WHERE patente = ? AND usuario_id = ?
+        ''', (patente, usuario_id))
+        vehiculo = self.cursor.fetchone()
+
+        if vehiculo:
+            # Registrar la solicitud de estacionamiento
+            self.cursor.execute('''
+                INSERT INTO estacionamientos (direccion, usuario_id, vehiculo_id)
+                VALUES (?, ?, ?)
+            ''', ("Dirección de estacionamiento", usuario_id, vehiculo[0]))
+            self.conn.commit()
+            print("Solicitud de estacionamiento realizada con éxito.")
+        else:
+            print("No se encontró ningún vehículo registrado con esa patente para este usuario.")
+
+    # Función para registrar un estacionamiento
+    def registrar_estacionamiento(self,direccion, discapacitados=False, disponible=False, usuario_id=None, vehiculo_id=None):
+        self.cursor.execute('''
+            INSERT INTO estacionamientos (direccion, Discapacitados, Disponible, usuario_id, vehiculo_id)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (direccion, discapacitados, disponible, usuario_id, vehiculo_id))
+        self.conn.commit()
+        print("Estacionamiento registrado exitosamente")
+
+    # Funcion editar usuario
+    def editar_usuario(self, id_usuario, nuevo_nombre, nuevo_apellido, nuevo_email):
+        self.cursor.execute("""
+        UPDATE usuarios SET nombre = ?, apellido = ?, email = ?
+        WHERE id = ?;""", (nuevo_nombre, nuevo_apellido, nuevo_email, id_usuario))
+        self.conn.commit()
+
+    # Funcion obtener datos del usuario
+    def obtener_datos(self, id_usuario):
+        self.cursor.execute("""SELECT nombre, apellido, email FROM usuarios WHERE id = ?;""", (id_usuario,))
+        return self.cursor.fetchone()
+    # Funcion obtener id del usuario
+    def obtener_id_usuario(self, email):
+        self.cursor.execute("SELECT id FROM usuarios WHERE email = ?;", (email))
+        return self.cursor.fetchone()
+
+    def eliminar_usuario(self, id_usuario):
+        try:
+            # Eliminar el usuario de la tabla de usuarios
+            self.cursor.execute('DELETE FROM usuarios WHERE id = ?', (id_usuario,))
+            # Eliminar los registros relacionados en otras tablas, como vehículos asociados
+            self.cursor.execute('DELETE FROM vehiculos WHERE usuario_id = ?', (id_usuario,))
+            # Eliminar los registros relacionados en la tabla de administradores, si corresponde
+            self.cursor.execute('DELETE FROM administradores WHERE usuario_id = ?', (id_usuario,))
+            # Commit los cambios
+            self.conn.commit()
+            print("Usuario eliminado exitosamente.")
+        except Exception as e:
+            print(f"Error al intentar eliminar el usuario: {e}")
+    def registrar_administrador(self, id_credenciales, nombre_completo, email, fecha_nacimiento, contraseña,
+                                usuario_id):
+        # Verificar si el administrador ya está registrado
+        self.cursor.execute('SELECT nombre_completo FROM administradores WHERE id_credenciales = ?', (id_credenciales,))
+        existente = self.cursor.fetchone()
+        if existente:
+            print(f"Error: El administrador {nombre_completo} ya está registrado.")
+            return
+
+        # Hashear la contraseña antes de almacenarla en la base de datos
+        contraseña_hasheada = hashlib.sha256(contraseña.encode()).hexdigest()
+        self.cursor.execute('''
+            INSERT INTO administradores (id_credenciales, nombre_completo, email, fecha_nacimiento, contraseña, usuario_id)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (id_credenciales, nombre_completo, email, fecha_nacimiento, contraseña_hasheada, usuario_id))
+
+        # Guardar los cambios
+        self.conn.commit()
+        print(f"El administrador {nombre_completo} se registró correctamente.")
+
+    def iniciar_sesion_administrador(self, id_credenciales, email, contraseña):
+        # Hashear la contraseña para compararla con la almacenada en la base de datos
+        contraseña_hasheada = hashlib.sha256(contraseña.encode()).hexdigest()
+
+        # Buscar al administrador en la base de datos
+        self.cursor.execute('''
+            SELECT nombre_completo FROM administradores 
+            WHERE id_credenciales = ? AND email = ? AND contraseña = ?
+        ''', (id_credenciales, email, contraseña_hasheada))
+        administrador = self.cursor.fetchone()
+
+        if administrador:
+            nombre_completo = administrador[0]
+            return f"Bienvenido, administrador {nombre_completo}. Sesión iniciada como administrador."
+        else:
+            return "Credenciales incorrectas para el administrador."
+    def obtener_id_admin(self, email):
+        self.cursor.execute("SELECT id FROM admininistradores WHERE email = ?;", (email))
+        return self.cursor.fetchone()
+
+    # Funcion para crear estacionamientos
+    def crear_estacionamiento(self, direccion, discapacitados=False, disponible=True):
+        try:
+            self.cursor.execute('''
+                INSERT INTO estacionamientos (direccion, Discapacitados, Disponible)
+                VALUES (?, ?, ?)
+            ''', (direccion, discapacitados, disponible))
+            self.conn.commit()
+            print(f"El espacio '{direccion}' creado exitosamente.")
+        except sqlite3.IntegrityError:
+            print(f"Error: El estacionamiento '{direccion}' ya existe.")
+        except Exception as e:
+            print(f"Ocurrió un error al intentar crear el estacionamiento: {e}")
+
+    # Funcion para que me de los estacionamientos
+    def obtener_estacionamientos(self):
+        self.cursor.execute('''
+            SELECT direccion, Disponible, COUNT(usuario_id) as Solicitudes
+            FROM estacionamientos
+            GROUP BY direccion
+        ''')
+        return self.cursor.fetchall()
+
+    # Funcion para actualizar de disponible a no
+    def actualizar_estado_estacionamiento(self, direccion, disponible):
+        self.cursor.execute('''
+            UPDATE estacionamientos SET Disponible = ?
+            WHERE direccion = ?
+        ''', (disponible, direccion))
+        self.conn.commit()
+
+    def actualizar_solicitud_estacionamiento(self, direccion):
+        # Aumenta el contador de solicitudes para un estacionamiento específico
+        self.cursor.execute('''
+            UPDATE estacionamientos SET solicitudes = solicitudes + 1
+            WHERE direccion = ?
+        ''', [direccion])
+        self.conn.commit()
+    def obtener_info_estacionamiento(self, direccion):
+        self.cursor.execute('''
+            SELECT direccion, Disponible FROM estacionamientos WHERE direccion = ?
+        ''', (direccion,))
+        return self.cursor.fetchone()
+
+    # Cerrar la conexión después de usar las funciones
+    def cerrar_conexion(self):
+        self.cursor.close()
+        self.conn.close()
+
+# Aqui para los ejemplos
+db = AccesoBaseDatos()
+#db.crear_usuario("Matias","Saa",20001211,"e","1234","HJKU22","Toyota","R-26")
+db.iniciar_sesion("e","1234")
+id_credenciales = 1
+nombre_completo = "Nombre Apellido"
+email = "admin@example.com"
+fecha_nacimiento = "1990-01-01"
+contraseña = "contraseña_segura"
+usuario_id = None
+
+# Llamar al método para registrar un nuevo administrador
+#db.registrar_administrador(id_credenciales, nombre_completo, email, fecha_nacimiento, contraseña, usuario_id)
+#id_credenciales = 1  # Asegúrate de que este ID coincida con el de un administrador existente
+#email = "admin@example.com"
+#contraseña = "contraseña_segura"
+
+# Llamar al método para iniciar sesión como administrador
+#mensaje = db.iniciar_sesion_administrador(id_credenciales, email, contraseña)
+#print(mensaje)
+
+print(db.obtener_estacionamientos())
+#Estacionamientos creados
+"""db.crear_estacionamiento("A1",False, True)
+db.crear_estacionamiento("A2",False, True)
+db.crear_estacionamiento("A3",False, True)
+db.crear_estacionamiento("A4",False, True)
+db.crear_estacionamiento("A5",False, True)
+db.crear_estacionamiento("A6",False, True)
+db.crear_estacionamiento("A7",False, True)"""
+
---
+++ main.py
@@ -0,0 +1,281 @@
+from kivy.properties import partial
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFlatButton
+from kivy.metrics import dp
+from kivymd.uix.textfield import MDTextField
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivymd.uix.screen import Screen
+from kivymd.uix.screenmanager import ScreenManager
+from kivymd.uix.navigationrail import MDNavigationRail, MDNavigationRailItem
+from kivymd.uix.list import MDList, OneLineListItem
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivymd.uix.toolbar import toolbar
+from kivy.uix.gridlayout import GridLayout
+from kivymd.uix.button import MDFlatButton
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+import requests
+
+from estacionamiento_basedatos import AccesoBaseDatos
+
+class DefaultScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    pass
+
+
+class RegisterScreen(Screen):
+    def registrarse(self):
+        print("Llegué aquí")
+class RegisterScreen2(Screen):
+    pass
+class proximamenteScreen(Screen):
+    pass
+class MenuScreen(Screen):
+    tarifa_actualizada = StringProperty("Tarifa Actual: 0")
+    def __init__(self, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+        self.estacionamientos_solicitados = set()
+        self.tiempo_recordatorio = 0
+
+
+    def on_enter(self, *args):
+        super(MenuScreen, self).on_enter(*args)
+        app = MDApp.get_running_app()
+        app.cargardatos()
+
+        self.mostrar_estacionamientos()
+
+    def mostrar_estacionamientos(self):
+        self.ids.container.clear_widgets()
+        estacionamientos = MDApp.get_running_app().db.obtener_estacionamientos()
+        layout = GridLayout(cols=2, spacing=10, size_hint_y=None, padding=dp(10))
+
+        for estacionamiento in estacionamientos:
+            boton = MDFlatButton(
+                text=f"{estacionamiento[0]}\nSolicitado por : {estacionamiento[2]} usuarios",
+                size_hint_y=None, height=dp(100),
+                md_bg_color=[1, 1, 0, 1] if estacionamiento[0] in self.estacionamientos_solicitados else [0, 1, 0, 1] if estacionamiento[1] else [1, 0, 0, 1]
+            )
+            boton.bind(on_release=partial(self.solicitar_estacionamiento, estacionamiento[0]))
+
+            layout.add_widget(boton)
+
+        self.ids.container.add_widget(layout)
+
+    def solicitar_estacionamiento(self, direccion, instance):
+        if direccion in self.estacionamientos_solicitados:
+            self.mostrar_popup("Error", f"Ya has solicitado el espacio {direccion}.")
+            return
+
+        if len(self.estacionamientos_solicitados) >= 3:
+            self.mostrar_popup("Error", "No puedes solicitar más de 3 espacios.")
+            return
+
+        self.estacionamientos_solicitados.add(direccion)
+        instance.md_bg_color = [1, 1, 0, 1]
+        self.mostrar_popup("Solicitud", f"Has solicitado el estacionamiento {direccion}.")
+        self.mostrar_popup_recordatorio(direccion)
+        self.mostrar_estacionamientos()
+
+    def mostrar_popup(self, titulo, mensaje):
+        popup_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        popup_label = Label(text=mensaje)
+        popup_button = MDFlatButton(text='Cerrar', on_release=lambda x: self.popup.dismiss())
+        popup_layout.add_widget(popup_label)
+        popup_layout.add_widget(popup_button)
+        self.popup = Popup(title=titulo, content=popup_layout, size_hint=(None, None), size=(400, 200))
+        self.popup.open()
+
+    def mostrar_popup_recordatorio(self, direccion):
+        content = BoxLayout(orientation='vertical')
+        label = Label(text=f'En cuántos minutos quieres ser recordado sobre la solicitud de {direccion}?')
+        time_input = MDTextField(input_filter='int', hint_text="Minutos")
+        button = Button(text="Confirmar", on_release=lambda x: self.configurar_recordatorio(time_input.text,direccion))
+        content.add_widget(label)
+        content.add_widget(time_input)
+        content.add_widget(button)
+        popup = Popup(title="Configurar Recordatorio", content=content, size_hint=(0.9, 0.4))
+        popup.open()
+
+    def configurar_recordatorio(self, tiempo,direccion):
+        try:
+            tiempo_en_minutos = int(tiempo)
+            Clock.schedule_once(
+                lambda dt: self.mostrar_popup("Recordatorio", f"Te recordamos que realizaste la solicitud de {direccion} aún se encuentra disponible."),
+                tiempo_en_minutos * 60)
+            self.mostrar_popup("Recordatorio Configurado", f"Serás notificado en {tiempo_en_minutos} minutos.")
+        except ValueError:
+            self.mostrar_popup("Error", "Introduce un número válido.")
+
+
+class AdminLoginScreen(Screen):
+    pass
+class EstacionamientoApp(MDApp):
+    admin_actu = None
+    usuario_actu = None
+    def build(self):
+        self.db = AccesoBaseDatos()
+        self.sm = ScreenManager()
+
+        Builder.load_file("estilo.kv")
+
+
+        self.sm.add_widget(DefaultScreen(name='default'))
+        self.sm.add_widget(LoginScreen(name='login'))
+        self.sm.add_widget(RegisterScreen(name='register'))
+        self.sm.add_widget(MenuScreen(name='menu'))
+        self.sm.add_widget(AdminLoginScreen(name='admin_login'))
+        self.sm.add_widget(proximamenteScreen(name='proximamente'))
+        self.sm.add_widget(RegisterScreen2(name='register2'))
+
+        return self.sm
+    def cambiar_pantalla(self, nombre_pantalla):
+        self.sm.current = nombre_pantalla
+        pass
+    def iniciar_sesion(self, email, password):
+        usuario_id = self.db.iniciar_sesion(email,password)
+        if usuario_id:
+            self.usuario_actu = usuario_id
+            self.cambiar_pantalla('menu')
+
+        else:
+            self.mostrar_popupLocal("Credenciales Invalidas","No se ha encontrado su usuario. Intentelo de nuevo.")
+        pass
+
+    def registrarse(self):
+        self.sm.current = 'register'
+        pass
+    def cargardatos(self):
+        if self.usuario_actu:
+            datos = self.db.obtener_datos(self.usuario_actu)
+            if datos:
+                menu_scren = self.root.get_screen('menu')
+                menu_scren.ids.nombre_usuario.text = datos[0]
+                menu_scren.ids.apellido_usuario.text = datos[1]
+                menu_scren.ids.email_usuario.text = datos[2]
+
+    def guardar_cambios(self):
+        nombre = self.root.get_screen('menu').ids.nombre_usuario.text
+        apellido = self.root.get_screen('menu').ids.apellido_usuario.text
+        email = self.root.get_screen('menu').ids.email_usuario.text
+        self.db.editar_usuario(self.usuario_actu, nombre, apellido, email)
+        self.mostrar_popupLocal("Actualizacion Exitosa","Hemos actualizado tú información con los datos que nos entregaste, cuando vuelvas a iniciar sesion recuerda ingresar con tus nuevas credenciales")
+
+    def iniciar_sesion_admin(self, credentials, email, password):
+        
+        admin_actu = self.db.iniciar_sesion_administrador(credentials,email,password)
+        if admin_actu:
+            self.admin_actu = admin_actu
+            self.mostrar_popupLocal("Bienvenido Administrado","Es un gusto volver a verlo Administrador. Que tenga un gran día")
+        else:
+            print("Credenciales inválidas para iniciar sesión como administrador")
+    def cerrar_sesion_usuario(self):
+        self.usuario_actu = None
+        self.mostrar_popupLocal("Sesión Cerrada","Se ha cerrado sesión.")
+        self.cambiar_pantalla('default')
+    def cerrar_sesion_admin(self):
+        self.admin_actu = None
+        self.cambiar_pantalla('default')
+    def eliminar_usuario(self):
+        if self.usuario_actu:
+            self.db.eliminar_usuario(self.usuario_actu)
+            self.mostrar_popupLocal("Usuario eliminado","Tu cuenta ha sido eliminada exitosamente. Espero hayas disfrutado tú tiempo con nosotros. Hasta luego!")
+            # Cerrar sesión del usuario después de eliminarlo
+            self.cerrar_sesion_usuario()
+        else:
+            print("No hay usuario conectado para eliminar")
+    def registrar_administrador(self, credentials, email, password):
+        
+        if self.db.registrar_administrador(credentials, email, password):
+            print("Registro de administrador exitoso")
+           
+        else:
+            print("El email ya está registrado")
+    def registrar_usuario(self):
+        # Obtiene los textos de los campos de contraseña
+        contrasena = self.root.get_screen('register').ids.password_field.text
+        confirmar_contrasena = self.root.get_screen('register').ids.Confirm_password_field.text
+        correo_electronico = self.root.get_screen('register').ids.email_field.text
+        confirmar_correo_electronico = self.root.get_screen('register').ids.email_confirm_field.text
+        color_normal = self.theme_cls.primary_color
+
+        hay_error = False
+
+        # Verifica si las contraseñas coinciden
+        if contrasena != confirmar_contrasena:
+            # Actualiza el color del texto a rojo si no coinciden
+            self.root.get_screen('register').ids.password_field.line_color_normal = self.theme_cls.error_color
+            self.root.get_screen('register').ids.Confirm_password_field.line_color_normal = self.theme_cls.error_color
+        else:
+            self.root.get_screen('register').ids.password_field.line_color_normal = color_normal
+            self.root.get_screen('register').ids.Confirm_password_field.line_color_normal = color_normal
+
+        if correo_electronico != confirmar_correo_electronico:
+            # Actualiza el color del texto a rojo si no coinciden
+            self.root.get_screen('register').ids.email_field.line_color_normal = self.theme_cls.error_color
+            self.root.get_screen('register').ids.email_confirm_field.line_color_normal = self.theme_cls.error_color
+            hay_error = True
+        else:
+            self.root.get_screen('register').ids.email_field.line_color_normal = color_normal
+            self.root.get_screen('register').ids.email_confirm_field.line_color_normal = color_normal
+
+        if not hay_error:
+            nombre = self.root.get_screen('register').ids.Name_field.text
+            apellido = self.root.get_screen('register').ids.Last_name_field.text
+            Edad = self.root.get_screen('register2').ids.Ageofuser.text
+            patente = self.root.get_screen('register2').ids.Patente_field.text
+            marca = self.root.get_screen('register2').ids.Marca_field.text
+            modelo = self.root.get_screen('register2').ids.Modelo_field.text
+            self.db.crear_usuario(nombre, apellido, 20201212, correo_electronico, contrasena, patente, marca, modelo)
+            self.iniciar_sesion(correo_electronico,contrasena)
+            pass
+
+        else:
+            self.root.current = 'register'
+    def ingreso_patente(self):
+        print("Ingresa patente")
+        pass
+    def volver_inicio(self):
+        self.sm.current = 'default'
+        pass
+    def volver_menu(self):
+        self.sm.current = 'menu'
+        pass
+    def proximamente(self):
+        self.sm.current = 'proximamente'
+
+    def mostrar_popupLocal(self, titulo, mensaje):
+        # Crear el contenido del popup
+        contenido = BoxLayout(orientation='vertical')
+        contenido.add_widget(Label(text=mensaje))
+        boton_cerrar = Button(text="Cerrar")
+        contenido.add_widget(boton_cerrar)
+
+        # Crear el popup
+        popup = Popup(title=titulo, content=contenido, size_hint=(None, None), size=(400, 200))
+
+
+        boton_cerrar.bind(on_release=popup.dismiss)
+
+        # Mostrar el popup
+        popup.open()
+
+    def actualizar_tarifa(self, *args):
+        # Supongamos que esta función genera o recupera una nueva tarifa
+        nueva_tarifa = "Tarifa Actual: 2"  # Aquí implementarías la lógica para obtener la nueva tarifa
+        self.root.get_screen('menu').tarifa_actualizada = nueva_tarifa
+
+    def on_start(self):
+        # Programa la función actualizar_tarifa para que se ejecute cada 60 segundos (1 minuto)
+        Clock.schedule_interval(self.actualizar_tarifa, 60)
+
+
+if __name__ == '__main__':
+    EstacionamientoApp().run()
